c62a9b4fe8c5a7cd5ae2ea0c0e3ee2c4d530f4da
Tjiho Lun <tom.darboux@eisti.fr>
2018-02-12 12:42:08 +0100

    factorize code and add comment

--- anderson.js
+++ anderson.js
@@ -7,11 +7,11 @@ anderson.getMsg = function(message,first_name,last_name,channel)
 	if(message == "yop")	
 		anderson.sendMsg("yop",channel);
 	else if(anderson.isQuestion(message))
-		anderson.sendMsg("hum... bonne question",channel)
+		anderson.sendMsg("oui oui certainement",channel)
 }
 anderson.sendMsg = function(message,channel)
 {
-	Request('https://api.telegram.org/'+Config.botKey+'/sendMessage?chat_id='+channel+'&text="'+message+'"', 
+	Request('https://api.telegram.org/'+Config.botKey+'/sendMessage?chat_id='+channel+'&text='+message, 
 		function (error, response, body) 
 		{
   			if (!error && response.statusCode == 200) {
@@ -21,12 +21,23 @@ anderson.sendMsg = function(message,channel)
 	)
 }
 
-function isYourWord(array,ele)
+//search if <word> match with one of words in <list_words_search>
+function isYourWord(list_words_search,word)
 {
-	return array.indexOf(ele) > -1
+	return list_words_search.indexOf(word) > -1
 }
 
-function applyRules(ele,list_rules)
+//search if one of <list_words_search> are in <list_words_message>
+function wordInYourMessage(list_words_message,list_words_search)
+{
+	return list_words_message.find((word) => 
+	{
+		return isYourWord(list_words_search,word)
+	}) != null
+}
+
+//return true if one rules is correct with ele
+function applyRulesOr(ele,list_rules)
 {
 	var res = false
 	list_rules.forEach(rule => {
@@ -39,26 +50,38 @@ function applyRules(ele,list_rules)
 	return res
 }
 
-anderson.isQuestion = function(message)
+//return true if all rules are correct with ele
+function applyRulesAnd(ele,list_rules)
 {
-	var list_words = message.split(" ")
-	var rules = []
-	
-	rules.push((array) => {
-		return array.find((ele) => 
+	var res = true
+	list_rules.forEach(rule => {
+		if(! rule(ele))
 		{
-			return isYourWord(["qui","comment","quel","quoi"],ele)
-		}) != null
-	})
-	rules.push(
-		(array) => message.indexOf("?") > -1
-	)
-	rules.push((array) => isYourWord(["je","tu","il","nous","vous","ils"],array[1]))
+			res = false
+		}
+	});
 
-	return applyRules(list_words,rules)
+	return res
 }
 
+//test if message seems to be a question
+anderson.isQuestion = function(message)
+{
+	var rules = []
+	
+	rules.push((list_words) => 
+		wordInYourMessage(list_words,["qui","comment","quel","quoi"])
+	)
 
+	rules.push(
+		(list_words) => message.indexOf("?") > -1
+	)
 
+	rules.push(
+		(list_words) => isYourWord(["je","tu","il","nous","vous","ils"],list_words[1])
+	)
+
+	return applyRulesOr(message.split(" "),rules)
+}
 
 module.exports = anderson;
