1a8f5737347dd201f83d7ea570f1ed69a6521150
Tjiho Lun <tom.darboux@eisti.fr>
2018-04-02 00:21:58 +0200

    fix telegram bot

--- bot-telegram.js
+++ bot-telegram.js
@@ -1,62 +1,28 @@
-const Express = require('express')
-const Server = Express()
-const Config = require('./static values/config')
-const BodyParser = require('body-parser')
-const Anderson = require('./src/anderson')
-const Log = require('./src/log')
+const Cmds = require("./cmd")
+const config = require("./static values/config")
+const TeleBot = require('telebot');
+const bot = new TeleBot(config.botKey);
 
-Server.use(BodyParser.json()); // for parsing application/json
-Server.use(BodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-Server.post('/update/'+Config.key,(req, res) =>
+bot.on('text', (msg) => 
 {
-        Log.log("request args:",req.body)
-        var message = req.body.message.text
-        var ok = false
-        var channel = req.body.message.chat.id
-        if (message.substring(0, 1) == '~') 
-        {
-            message = message.slice(1)
-            for(cmd_name in Cmds)
-            {
-                if(Cmds[cmd_name].test(message))
-                {
-                    Cmds[cmd_name].action(message,(msg) => send(msg,channel),(msg) => send(msg,channel))
-                    return true
-                }    
-            }
-
-            Cmds["default"].action(message,(msg) => reply(e,msg),(msg) => send(e,msg))
-        }
-    	
-    		//derson.getMsg(req.body.message.text, req.body.message.from.first_name, req.body.message.from.last_name, req.body.message.chat.id)
-})
-
-
-Server.get('/', (req, res) =>
-{
-        res.send('Hello World!')
-})
-
-Server.listen(Config.port, () => 
-{
-    console.log('Anderson server is listening on port '+Config.port)
-})
-
-function send(message,channel)
-{
-	Request.post(
+	var message = msg.text
+	console.log(message)
+	var ok = false
+    	if (message.substring(0, 1) == '~') 
+    	{
+        	message = message.slice(1)
+		for(cmd_name in Cmds)
 		{
-			'uri':'https://api.telegram.org/'+Config.botKey+'/sendMessage', 
-			form:{"chat_id":channel,"text":message},
-			'encoding':'utf-8',
-			headers: {
-    				"Content-Type":"application/json; charset=utf-8"
-  			}
-		},
-		function (error, response, body) 
-		{
-  			console.log(error)
+			if(Cmds[cmd_name].test(message))
+			{
+			    Cmds[cmd_name].action(message,msg.reply.text,msg.reply.text)
+			    return true
+			}    
 		}
-	)
-}
+
+		Cmds["default"].action(message,msg.reply.text,msg.reply.text)
+	}	
+	
+})
+
+bot.start();    	
