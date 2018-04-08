b6e58e1f3ff5d3935df89b9b5552c861caad5178
Tjiho Lun <tom.darboux@eisti.fr>
2018-04-08 21:58:37 +0200

    add random answer

--- bot-discord.js
+++ bot-discord.js
@@ -4,7 +4,16 @@ var client = new Discordie();
 const config = require("./static values/config")
 const Cmds = require("./cmd")
 const Cmds_embed = require("./cmd_discord")
-
+const fs = require("fs")
+function getRandomLine(filename,e){
+  fs.readFile(filename, function(err, data){
+    if(err) throw err;
+    data+=""
+    var lines = data.split('\n');
+    
+    reply(e,lines[Math.floor(Math.random()*lines.length)]);
+ })
+}
 
 client.connect({ token: config.discord });
 
@@ -16,10 +25,27 @@ client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
     
     var message = e.message.content
     var ok = false
-    if (message.substring(0, 1) == '~') 
+    
+   // cosolee.message.author.username = "tjiho)
+	
+   // console.log(message.indexOf("<@424318724242407424>"),message)
+
+    if (message.substring(0, 1) == '~' || message.indexOf("<@424318724242407424>") > -1) 
     {
-        message = message.slice(1)
-        try
+        if (message.substring(0, 1) == '~') 
+		message = message.slice(1)
+        
+
+        if (message.substring(0, 3) == '<@4')
+		message = message.slice(22) 
+		
+	// console.log(message) 
+    	if(e.message.author.username == "Krysthalia")
+	{
+		getRandomLine("random.txt",e)
+	//	return 5
+	}
+	try
         {
             for(cmd_name in Cmds)
             {
