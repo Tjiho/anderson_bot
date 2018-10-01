54e08ef937d24da6a75e7f39118bcdb57e28f8ef
Tjiho Lun <tom.darboux@eisti.fr>
2018-10-01 15:23:10 +0200

    change ok comd

--- cmd/ok.js
+++ cmd/ok.js
@@ -2,7 +2,7 @@ const Check = require("../share/check")
 
 exports["action"] = function(message,f_reply,f_send,client)
 {
-    f_send("!ok")
+    f_send("https://tetris.weloveminitel.eu/upload/Screenshot_20181001_115509.png")
 }
 
 exports["test"] = (message) => Check.checkCmd(message,["ok"])
