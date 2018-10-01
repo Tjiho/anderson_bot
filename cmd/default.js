1f4b6b19e6e25d38d6eb8d9a5ed84305c6f11d82
Tjiho Lun <tom.darboux@eisti.fr>
2018-10-01 15:23:37 +0200

    fix default cmd, anderson replace @anderson with I

--- cmd/default.js
+++ cmd/default.js
@@ -8,9 +8,13 @@ exports["action"] = function(message,f_reply,f_send,client)
     if(args.length > 0)
     {
         if(cmd[cmd.length -1] == "e")
-            f_send(args.join(" ")+" was "+cmd+"d");
+        {
+		result = args.join(" ")+" was "+cmd+"d"
+		result = result.replace("<@424318724242407424>","I")
+		f_send(result)
+	}
         else
-            f_send(args.join(" ")+" was "+cmd+"ed");
+            f_send((args.join(" ")+" was "+cmd+"ed").replace("<@424318724242407424>","I"));
     }
     else
         f_reply("type `~help` or `~aide` or `~?`")
